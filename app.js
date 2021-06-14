5bfa636fac81a884a935a66043b2cbbae79d77a6
francopuma95 <francopuma.d95@gmail.com>
2021-06-14 16:14:18 -0300

    mostrar

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const publicpath = path.resolve(__dirname,'./public');
 
 app.use(express.static(publicpath));
   
-app.listen(process.env.PORT || 3002, ()=> { 
-console.log("Levantando un servidor con 3002");
+app.listen(process.env.PORT || 3000, ()=> { 
+console.log("Levantando un servidor con 3000");
  }) 
 
 
  app.get('/',(req,res ) => {
-    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+    res.sendFile(path.resolve(__dirname,'./views/index.html'));
  } )
